86a28c6a2b0e8b917ec5ebccd36f3c991e2b3bbb
Jerzy Ledzinski <jurekledzinski@yahoo.pl>
2020-06-16 11:23:54 +0200

    Updates

---
+++ precache-manifest.40e121c635ca5b0cbb957d76ce35e57a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1da105fd9d41c96e4e2a",
+    "url": "/app-react-router/static/css/main.f82a7cf0.chunk.css"
+  },
+  {
+    "revision": "1da105fd9d41c96e4e2a",
+    "url": "/app-react-router/static/js/main.9c340488.chunk.js"
+  },
+  {
+    "revision": "d466ce07c2972432d57f",
+    "url": "/app-react-router/static/js/runtime~main.6a4469d8.js"
+  },
+  {
+    "revision": "a5bb709e81a207ac67bf",
+    "url": "/app-react-router/static/js/2.227170bb.chunk.js"
+  },
+  {
+    "revision": "148276c6be7fd5fc652897d783207619",
+    "url": "/app-react-router/static/media/forest.148276c6.jpg"
+  },
+  {
+    "revision": "f69ea073df286a2c063dc172f0426ca7",
+    "url": "/app-react-router/static/media/road.f69ea073.jpg"
+  },
+  {
+    "revision": "c48e59a8808f978d3f4561e0870622fa",
+    "url": "/app-react-router/static/media/beach.c48e59a8.jpg"
+  },
+  {
+    "revision": "c6f76a3be3d092c105bd68f4c05f4b97",
+    "url": "/app-react-router/static/media/field.c6f76a3b.jpg"
+  },
+  {
+    "revision": "25be0d31f2f5d11fadd49276f37da443",
+    "url": "/app-react-router/index.html"
+  }
+];
